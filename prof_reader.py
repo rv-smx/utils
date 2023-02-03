b731850bd206f6c4c0c5e23b27dbd6f71a98c3ed
MaxXSoft <x@MaxXSoft.net>
2023-02-03 18:44:18 +0800

    (prof_reader) supported dumping to JSON

--- prof_reader.py
+++ prof_reader.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Callable
+from typing import Callable, Dict, Any
 import struct
 import sys
 import subprocess
+import json
 
 
 RECORD_FMT = '@Pdd'
@@ -16,39 +17,60 @@ unpack_relocation = struct.Struct(RELOCATION_FMT).unpack_from
 
 
 LocQuery = Callable[[int], str]
+Record = Dict[str, Any]
 
 
-def print_record(i: int, rel: int, query: LocQuery, data: bytes) -> None:
+def get_record(rel: int, query: LocQuery, data: bytes) -> Record:
   addr, read_mpki, write_mpki = unpack_record(data)
   rel_addr = addr - rel
+  return {
+      'address': hex(rel_addr),
+      'location': query(rel_addr),
+      'readMpki': read_mpki,
+      'writeMpki': write_mpki,
+  }
+
+
+def print_record(i: int, record: Record) -> None:
   print(f'Loop #{i}:')
-  print(f'  Address: {hex(rel_addr)}')
-  print(f'  Location: {query(rel_addr)}')
-  print(f'  Cache Read MPKI: {read_mpki:.4f}')
-  print(f'  Cache Write MPKI: {write_mpki:.4f}')
+  print(f'  Address: {record["address"]}')
+  print(f'  Location: {record["location"]}')
+  print(f'  Cache Read MPKI: {record["readMpki"]:.4f}')
+  print(f'  Cache Write MPKI: {record["writeMpki"]:.4f}')
 
 
 if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    print(f'usage: {sys.argv[0]} BINARY PROF_FILE')
-    exit(1)
-
-  bin = sys.argv[1]
-  prof = sys.argv[2]
+  import argparse
+  parser = argparse.ArgumentParser(
+      description='Loop profile reader.')
+  parser.add_argument('binary', type=str, help='binary file with debug info')
+  parser.add_argument('profile', type=str, help='loop profile')
+  parser.add_argument('-j', '--json', default=False, action='store_true',
+                      help='dump JSON')
+  args = parser.parse_args()
 
   # define the location query function
   def query(addr: int) -> str:
-    ret = subprocess.run(['addr2line', '-e', bin, hex(addr - 1)],
+    ret = subprocess.run(['addr2line', '-e', args.binary, hex(addr - 1)],
                          capture_output=True, text=True)
     return 'Unknown' if ret.returncode else ret.stdout.strip()
 
   # read the profile data
-  with open(prof, 'rb') as f:
+  records = []
+  with open(args.profile, 'rb') as f:
     # get relocation
     rel_data = f.read(RELOCATION_LEN)
     rel = unpack_relocation(rel_data)[0]
     # print records
     i = 0
     while data := f.read(RECORD_LEN):
-      print_record(i, rel, query, data)
+      record = get_record(rel, query, data)
+      if args.json:
+        records.append(record)
+      else:
+        print_record(i, record)
       i += 1
+
+  # dump JSON
+  if args.json:
+    json.dump(records, sys.stdout)
